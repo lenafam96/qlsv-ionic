c0f2957855a4f84c4405bf42f3b7bedf2d1d1acb
lenafam96 <79802253+lenafam96@users.noreply.github.com>
2022-12-15 10:11:32 +0700

    Merge pull request #3 from lenafam96/Hai
    Hai

--- src/models/StudentArray.tsx
+++ src/models/StudentArray.tsx
@@ -2,3 +2,12 @@ import { Student } from "./Student";
 
 const StudentArray: Student[] = [];
 export { StudentArray };
+
+export function checkIdExists(id: string): boolean {
+  for (let i = 0; i < StudentArray.length; i++) {
+    if (StudentArray[i].getId() === id) {
+      return true;
+    }
+  }
+  return false;
+}
